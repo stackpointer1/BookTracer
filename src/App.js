897a636dc23d3a0d92e11c8260475d46d7f69697
stackpointer1 <76005177+stackpointer1@users.noreply.github.com>
2021-04-20 21:51:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import Header from  './Header';
+import Books from './Books';
+import './styles.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Header />
+        <Books/>
+      </div>
+    )
+  }
+} 
+
+export default App;
